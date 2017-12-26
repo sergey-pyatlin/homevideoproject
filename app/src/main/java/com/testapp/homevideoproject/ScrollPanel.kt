f37f7b74d2cb3324c513b766cd8943c6c267cdca
sergey-pyatlin <sergey.pyatlin@gmail.com>
2017-12-26 23:00:07 +0300

    LinearLayout with vertical orientation works fine

--- app/src/main/java/com/testapp/homevideoproject/ScrollPanel.kt
+++ app/src/main/java/com/testapp/homevideoproject/ScrollPanel.kt
@@ -41,24 +41,34 @@ class ScrollPanel : AppCompatActivity() {
         tvSwitcher.text = "Nexxt step of my changes - make "
         tvSwitcher.setWidth(600)
         tvSwitcher.setBackgroundColor(Color.BLUE)
-        //linearSwitcher.addView(lineOfSwitcher)
-    //    linearSwitcher.addView(lineOfSwitcher)
+
+        linearSwitcher.addView(lineOfSwitcher)
+        linearSwitcher.addView(tvSwitcher)
 
 //        val inflater = this@ScrollPanel.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
  //       val view = inflater.inflate(R.layout.carousel_container, linearSwitcher, false)
  //       linearSwitcher.addView(view)
         //val testLinearLayout: LinearLayout = findViewById(R.id.carousel_container) as LinearLayout
 
-        var ltInflater: LayoutInflater = layoutInflater
+/*        var ltInflater: LayoutInflater = layoutInflater
         var line : LinearLayout = LinearLayout(this@ScrollPanel)
 
         for (x in 1..10 step 1){
             val item : FrameLayout = ltInflater.inflate(R.layout.carousel_container, line, false) as FrameLayout
+
             val tvName = item.findViewById<TextView>(R.id.tvName)
             var strS:String = ">>> item #: $x"
+            if (x==1)
+                tvName.setBackgroundColor(Color.RED)
+            if (x==2)
+                tvName.setBackgroundColor(Color.YELLOW)
+            if (x==3)
+                tvName.setBackgroundColor(Color.BLUE)
             tvName.setText(strS)
             line.addView(item)
+
+
         }
-        linearSwitcher.addView(line)
+        linearSwitcher.addView(line)*/
     }
 }
